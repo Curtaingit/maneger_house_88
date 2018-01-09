62a8473526a61fe37b4e9183716695f3fbc54db1
curtain <1158380326@qq.com>
2018-01-09 13:58:14 +0800

    init  project

---
+++ src/main/java/com/example/manager_house_88/ManagerHouse88Application.java
@@ -0,0 +1,12 @@
+package com.example.manager_house_88;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerHouse88Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerHouse88Application.class, args);
+	}
+}
